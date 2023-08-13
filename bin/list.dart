cd4b6e65a48eff37e1e102f2560efb72e3b90812
Zahid Ayturan <91957947+zahidayturan@users.noreply.github.com>
2023-08-13 22:29:47 +0300

    Add files via upload

---
+++ bin/list.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+
+      List<String> operationTool = ["aaa","bbb","ccc"];
+      String operationTool1 = operationTool.length == 1 ?  operationTool[0] : '';
+      String operationTool2 = operationTool.length == 2 ?  operationTool[0] : '';
+      String operationTool3 = operationTool.length == 3 ?  operationTool[0] : '';
+      String operationTool4 = operationTool.length == 4 ?  operationTool[0] : '';
+      var now = DateTime.now();
+  var weekNumber = (31/ 7).ceil();
+  print(weekNumber);
+}
